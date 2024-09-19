f35cac55cec293b7c8f819ee7621f7525192e66a
Juliana Castanho Teixeira <julianact2004@gamail.com>
2024-09-18 22:09:26 -0300

    Iniciando Projeto

---
+++ JogoAdivinhacao.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class JogoAdivinhacao {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        int rand = random.nextInt(100);
+        int vida = 5;
+        while (vida > 0) {
+            
+        }
+        System.out.println("Você chegou a 0 vidas :(");
+    }
+}
