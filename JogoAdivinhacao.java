9b62c6bbab19cbf8465d3ec7fc516d7db6860174
Juliana C. Teixeira <julianact2004@gmail.com>
2024-09-20 02:29:34 -0300

    Adicionei sistema de tentativas, ele mostra para o usuario se o numero chutado esta acima ou abaixo do numero rand e se esta quente ou frio, quente sendo uma diferença <= a 10

--- JogoAdivinhacao.java
+++ JogoAdivinhacao.java
@@ -7,10 +7,27 @@ public class JogoAdivinhacao {
         Scanner scan = new Scanner(System.in);
         Random random = new Random();
         int rand = random.nextInt(100);
-        int vida = 5;
-        while (vida > 0) {
-            
+        int tentativas = 0;
+        while (tentativas < 6) {
+            System.out.println("Digite um numero:");
+            int num = scan.nextInt();
+            tentativas++;
+            System.out.println(tentativas);
+            if (num != rand) {
+                if (num < rand) {
+                    System.out.println("Esse numero é menor que " + rand);
+                    System.out.println((rand - 10) <= num ? "Quente" : "Frio");
+                }else if (num > rand) {
+                    System.out.println("Esse numero é menor que " + rand);
+                    System.out.println((rand + 10) >= num ? "Quente" : "Frio");
+                }
+            }else{
+                System.out.println("Parabêns você venceu em " + tentativas + " tentativas!");
+                break;
+            }
         }
-        System.out.println("Você chegou a 0 vidas :(");
+        if (tentativas == 6) {
+            System.out.println("Você gastou todas as suas tentativas :(");
+        }
     }
 }
